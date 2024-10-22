dd611c2613156c5cbfd558749475df4b50bf785d
Jepierre89 <jeanpierre.ortiz@coins-colombia.com>
2024-10-22 15:41:53 -0500

    datasource aws

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-const PORT = process.env.PORT || 3005
+const PORT = process.env.PORT || 3006
 
 function main(){
   app.listen(PORT, ()=>  console.log(`Server is running on port ${PORT}`));
--- src/db.js
+++ src/db.js
@@ -2,10 +2,10 @@ import { createConnection, createPool } from "mysql2/promise";
 
 
 const pool = createPool({
-  host: "mysqldb",
+  host: "database-1.czuseiamuhpc.us-east-2.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password:'root'
+  user: "admin",
+  password:'admin123456'
 })
 
 export default pool 
